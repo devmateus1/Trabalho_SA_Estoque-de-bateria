c8ece7cc88cf9aef524a86c7e39ca4e28e49d3f2
maxmanoel <max329900@gmail.com>
2025-03-10 21:23:34 -0300

    Update tela_de_adm.py

--- tela_de_adm.py
+++ tela_de_adm.py
@@ -1,32 +1,26 @@
 from tkinter import * #Importa todos os mudulos do tkinter
 from tkinter import messagebox # Importar o mudulo de widgets tematicos do tkinter
 from tkinter import ttk
+
 #from DataBase import DataBase
 
 # Criar a janela
-jan = Tk()
-jan.title("Tela de adm - Painel de Acesso")
-jan.geometry("600x300")
-jan.configure(background="#002333")
-jan.resizable(width=False, height=False)
+class TeldACASTRO:
+    jan = Tk()
+    jan.title("Tela de adm - Painel de Acesso")
+    jan.geometry("600x300")
+    jan.configure(background="#002333")
+    jan.resizable(width=False, height=False)
 
 
 
-Produtolabel = Label (text="PRODUTO:", bg="#002333" , fg="white")
-Produtolabel.place (x=160 , y=70)
-ProdutoEntry = ttk.Entry (width=15)
-ProdutoEntry.place (x=250 ,y=70)
+    Produto = Button (text="PRODUTO", width=10)
+    Produto.place (x=100 ,y=30)
 
-Fornecedorlabel = Label (text="FORNECEDOR:", bg="#002333" , fg="white")
-Fornecedorlabel.place (x=160 , y=100)
-FornecedorEntry = ttk.Entry (width=15)
-FornecedorEntry.place (x=250 ,y=100)
+    Fornecedor = Button (text="FORNECEDOR", width=15)
+    Fornecedor.place (x=200 ,y= 30)
 
-
-Funcionariolabel = Label (text="FUNCIONARIO:", bg="#002333" , fg="white")
-Funcionariolabel.place (x=160 , y=130)
-FuncionarioEntry = ttk.Entry (width=15)
-FuncionarioEntry.place (x=250 ,y=130)
-
-
-jan.mainloop()
+    Funcionario = Button (text="FUNCIONARIO", width=15)
+    Funcionario .place (x=350 ,y=30)
+   
+    jan.mainloop()
