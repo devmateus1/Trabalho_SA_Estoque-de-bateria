26cc184fbe787243d802086987e55d5343006b14
maxmanoel <max329900@gmail.com>
2025-03-13 22:13:53 -0300

    alteracao tela de login

--- tela_de_usuario.py
+++ tela_de_usuario.py
@@ -2,6 +2,7 @@ from tkinter import * #Importa todos os mudulos do tkinter
 from tkinter import messagebox # Importar o mudulo de widgets tematicos do tkinter
 from tkinter import ttk
 
+<<<<<<< HEAD
 import DataBase
 
 #  Criar a janela
@@ -46,3 +47,29 @@ class TeldACASTRO:
 root = Tk()
 tela = TeldACASTRO(root)
 root.mainloop()
+=======
+ #from DataBase import DataBase
+
+#Criar a janela
+class TeldACASTRO:
+    jan = Tk()
+    jan.title("Tela de usuario - Painel de Acesso")
+    jan.geometry("600x300")
+    jan.configure(background="#002333")
+    jan.resizable(width=False, height=False)
+
+    text1 = Label (text="Tela Usuario", width=49)
+    text1.place (x=100 ,y=20)
+
+    Produto = Button (text="PRODUTO", width=10)
+    Produto.place (x=100 ,y=50)
+
+    Fornecedor = Button (text="FORNECEDOR", width=15)
+    Fornecedor.place (x=200 ,y= 50)
+
+
+    Funcionario = Button (text="FUNCIONARIO", width=15) 
+    Funcionario .place (x=335 ,y=50)
+   
+    jan.mainloop()
+>>>>>>> f7da4dec606c82da5fbe97b22081c023e551e592
